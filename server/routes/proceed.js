6a07c085339f9af9256224fb3fa75d607a7e6b47
Arnaud <arnaud.dassy@gmail.com>
2016-01-19 08:56:12 +0100

    gg

--- server/routes/proceed.js
+++ server/routes/proceed.js
@@ -10,12 +10,11 @@ exports.init = function( oServerInstance, inventory ) {
     oServerInstance.get( "/step_one", function( oRequest, oResponse ) {
         if(oRequest.cookies.userID){
             oResponse.redirect("http://localhost:12345/step_two");
-        };
+        }
         oResponse.render( "./pages/proceed/step_one.jade" );
     } );
     
     oServerInstance.get( "/step_two", function( oRequest, oResponse ) {
-        //TODO : Check if user already gave an url for trade (through data base)
         oResponse.render( "./pages/proceed/step_two.jade" );
     } );
     
